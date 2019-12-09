900436b05240228190e4e273145d9005a93d0511
Paulo Barcelos <Paulo.barcelos@hotmail.com>
2019-12-09 20:26:28 -0300

    configurado o envio de Email ao criar um novo Plano

--- src/app/controllers/registrationController.js
+++ src/app/controllers/registrationController.js
@@ -1,7 +1,10 @@
 import Registration from '../models/Registration';
 import Plans from '../models/Plans';
-import { addMonths, parseISO} from 'date-fns';
+import Students from '../models/Students';
+import { addMonths, parseISO, format } from 'date-fns';
 import * as Yup from 'yup';
+import Mail from '../../lib/Mail';
+import  pt  from 'date-fns/locale/pt';
 class registrationController{
   async store(req,res){
     const schema = Yup.object().shape({
@@ -13,9 +16,18 @@ class registrationController{
       return res.status(400).json({ error: ' falha no preenchimento dos dados'});
     }
     const { students_id, plans_id, start_date} = req.body;
+    const checkStudent = await Registration.findOne({
+      where:{students_id: students_id}
+    });
+    if(checkStudent){
+      return res.status(400).json({ error: ' O estudante já esta matriculado! '});
+    }
     const refplano = await Plans.findOne(
     {
-      where:{id: plans_id}
+      where:{id: plans_id},
+    });
+    const refstudent = await Students.findOne({
+      where:{id: students_id},
     });
     const end_date = addMonths(parseISO(start_date), refplano.duration);
     const total_price = refplano.duration * refplano.price;
@@ -25,7 +37,26 @@ class registrationController{
       start_date,
       end_date,
       total_price,
-    })
+    });
+     await Mail.sendMail({
+      to: `${refstudent.name}<${refstudent.email}>` ,
+      subject: 'Matricula Realizada',
+      template: 'registration',
+      context:{
+        students: refstudent.name,
+        plan: refplano.title,
+        start_date:  format (parseISO(start_date), "'dia' dd ' mes ' MMMM ' ano 'yyyy'",{
+          locale: pt,
+        }),
+        end_date: format (end_date, "'dia' dd ' mes ' MMMM ' ano 'yyyy'",{
+          locale: pt,
+        }),
+        plan_valor: refplano.price,
+        total_valor: total_price,
+      },
+    });
+
+
     return res.json(registration);
   }
 }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user:'643fbf626f545e',
+    pass:'fc33baf33808e4',
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gobarber.com>',
+  },
+};
