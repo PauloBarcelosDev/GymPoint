989af87d29ae60d84a120ca75868cc4e0ee83d06
Paulo Barcelos <Paulo.barcelos@hotmail.com>
2019-12-19 17:16:39 -0300

    criação da migration da tatabela Checkins

---
+++ src/database/migrations/20191219195845-create-Checkins.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.createTable('Checkins', {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        students_id:{
+          type: Sequelize.INTEGER,
+          references: { model: 'students', key: 'id'},
+          onUpdate: 'CASCADE ',
+          onDelete: 'SET NULL',
+          allowNull: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+
+      });
+
+  },
+
+  down: queryInterface => {
+
+      return queryInterface.dropTable('Checkins');
+
+  },
+};
